91406d57c0d8c4132fdfb2a51a76aaca8a5fd553
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-09-24 14:02:55 +0800

    change webHistory

--- src/router/index.js
+++ src/router/index.js
@@ -48,8 +48,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  //history: createWebHashHistory(),
+  //history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
