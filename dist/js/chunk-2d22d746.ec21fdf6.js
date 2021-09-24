4e09247b903a00adfe73c1e9a353c825b996a52a
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-09-24 14:04:13 +0800

    deploy

--- dist/js/chunk-2d22d746.ec21fdf6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=t("7a23"),a={class:"about"},s=Object(c["i"])("h1",null,"This is an about page",-1);function u(n,e){return Object(c["r"])(),Object(c["e"])("div",a,[s])}const i={};i.render=u;e["default"]=i}}]);
-//# sourceMappingURL=chunk-2d22d746.ec21fdf6.js.map
